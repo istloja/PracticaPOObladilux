1d53d381b6c297f5b6267357f17d52a008abcd83
ist loja <ist loja@ISTLOJA03>
2019-06-20 10:57:35 +0200

    metodos estaticos y dinamicos

---
+++ PracticaPOObladix/src/metodos/EstaticosyDinamicos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+/**
+ *
+ * @author ist loja
+ */
+public class EstaticosyDinamicos {
+    public static void main(String[] args) {
+        saludar();// para llamra un metodo estatico
+        
+        // Si quiero llamar despedir debo crear un objeto
+        EstaticosyDinamicos objeto=new  EstaticosyDinamicos();// crear objeto
+        objeto.despedir();
+        
+    }
+    public static void saludar(){// Este es un metodo Estatico
+        System.out.println("Hola");
+    }
+     
+    public void despedir(){ // Este es un metodo No Estatico
+        System.out.println("Chao");
+    }
+    
+}
